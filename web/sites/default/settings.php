0fa21066f1d7ac98d48c9521d004db39ec2c52d4
kyahayamoussa <kyahayamoussa@INF-113.univic.fr>
2014-11-20 15:41:46 +0100

    modification du menu

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -215,7 +215,7 @@ $databases = array (
   array (
     'default' => 
     array (
-      'database' => 'c0sippenet',
+      'database' => 'sippe',
       'username' => 'root',
       'password' => '',
       'host' => 'localhost',
@@ -256,7 +256,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '9NrilEeSxIAGs7w3FqmnsJcyOlpGZTHAtGpS-cyLI2I';
+$drupal_hash_salt = 'En1SAAAIncvcmzNKcVmuO_d6Io3k85oKaD3YqaXSwwg';
 
 /**
  * Base URL (optional).
